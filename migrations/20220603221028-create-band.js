b3acf712f1040de4342bbd1a800836da9f7f3371
Anthony Babka <anthonybabka@gmail.com>
2022-06-03 16:50:16 -0700

    completed activities

--- migrations/20220603221028-create-band.js
+++ migrations/20220603221028-create-band.js
@@ -1,10 +1,23 @@
-'use strict';
-const { DataTypes } = require('sequelize');
+'use strict'
 
 module.exports = {
   up: async (queryInterface, Sequelize) => {
-    await queryInterface.addColumn('bands', 'recommendation', {
-        type: DataTypes.STRING
+    await queryInterface.addColumn('bands', {
+      band_id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      }, name: {
+        type: Sequelize.STRING,
+        allowNull: false,
+      },  genre: {
+        type: Sequelize.TEXT,
+        allowNull: false,
+      },available_start_time: {
+        type: Sequelize.DATE,
+        allowNull: false,
+      },
     })
   },
 
